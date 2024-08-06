83a860f23855bc8422c901ec59d8af0b3c745703
Sharvari Dhote <sharvaridhote6@gmail.com>
2024-08-06 06:06:13 +0530

    Commit

--- forloop.cpp
+++ forloop.cpp
@@ -1,5 +1,29 @@
 #include<iostream>
 using namespace std;
 int main(){
+/*
+    //sum of numbers 1 to n
+    int n;
+    cout<<"enter a number"<<endl;
+    cin>>n;
+    int sum=0;
+    for(int i=1;i<=n;i++){
+        sum=sum+i;
+    }
+    cout<<sum<<endl;
     
+
+
+   //fibonacci series- next number is sum of last 2 numbers
+    int n=10;
+    int a=0;
+    int b=1;
+    cout<<a<<" "<<b<<" ";
+    for(int i=1;i<=n;i++){
+        int nextNumber= a+b;
+        cout<<nextNumber<<" ";
+        a=b;
+        b=nextNumber;
+    }
+    */
 }
