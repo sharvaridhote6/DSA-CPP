c869d32e07619988454d4f944a896a72d1f16b11
Sharvari Dhote <dhotesharvari6@gmail.com>
2025-05-04 16:17:19 +0530

    push

--- C++ basics/practicebasics.cpp
+++ C++ basics/practicebasics.cpp
@@ -1,48 +1,47 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int a,b=1;
-    a=10;
-    if(++a){
-        cout<<b<<endl;
+int main()
+{
+    int a, b = 1;
+    a = 10;
+    if (++a)
+    {
+        cout << b << endl;
     }
-    else{
-        cout<<++b<<endl;
+    else
+    {
+        cout << ++b << endl;
     }
 
-
-
-    int x=1;
-    int y=2;
-    if(x-->0 && ++y>2){
-        cout<<"stage1-inside if";
+    int x = 1;
+    int y = 2;
+    if (x-- > 0 && ++y > 2)
+    {
+        cout << "stage1-inside if";
     }
-    else{
-        cout<<"stage2- inside else";
+    else
+    {
+        cout << "stage2- inside else";
     }
-    cout<<x<<" "<<y<<endl;
+    cout << x << " " << y << endl;
 
-
-
-    int r=1;
-    int s=2;
-    if(r-->0 || ++s>2){
-        cout<<"stage1-inside if";
+    int r = 1;
+    int s = 2;
+    if (r-- > 0 || ++s > 2)
+    {
+        cout << "stage1-inside if";
     }
-    else{
-        cout<<"stage2- inside else";
+    else
+    {
+        cout << "stage2- inside else";
     }
-    cout<<r<<" "<<s<<endl;
+    cout << r << " " << s << endl;
 
+    int number = 3;
+    cout << (25 * (++number)) << endl;
 
-
-    int number=3;
-    cout<<(25*(++number))<<endl;
-
-
-    int q=1;
-    int p=q++;
-    int f= ++q;
-    cout<<p<<" "<<f<<endl;
-
+    int q = 1;
+    int p = q++;
+    int f = ++q;
+    cout << p << " " << f << endl;
 }
