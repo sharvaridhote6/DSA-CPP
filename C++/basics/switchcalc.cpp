c7647957c18ad7dd8e53a762a1dc23f4b030f0a9
Sharvari Dhote <dhotesharvari6@gmail.com>
2025-05-05 19:20:05 +0530

    push

--- C++/basics/switchcalc.cpp
+++ C++/basics/switchcalc.cpp
@@ -17,6 +17,7 @@ int main()
     }
     cout << endl;
 
+
     // calculator
     int a, b;
     char op;
@@ -54,5 +55,35 @@ int main()
 
     default:
         cout << "Please enter a valid operator " << endl;
+    }
+    cout << endl;
+
+    
+        // rupee problem
+    int amount;
+    cin >> amount;
+    int Rs100, Rs50, Rs20, Rs1;
+
+    switch (1)
+    {
+    case 1:
+        Rs100 = amount / 100;
+        amount = amount % 100;
+        cout << "No of 100 Rupee =" << Rs100 << endl;
+
+    case 2:
+        Rs50 = amount / 50;
+        amount = amount % 50;
+        cout << "No of 50 Rupee =" << Rs50 << endl;
+
+    case 3:
+        Rs20 = amount / 20;
+        amount = amount % 20;
+        cout << "No of 20 Rupee =" << Rs20 << endl;
+
+    case 4:
+        Rs1 = amount / 1;
+        amount = amount % 1;
+        cout << "No of 1 Rupee =" << Rs1 << endl;
     }
 }
