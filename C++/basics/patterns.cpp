264a49b5b6b8e254c888b28107eb62ce0bab96f2
Sharvari Dhote <dhotesharvari6@gmail.com>
2025-05-18 18:53:25 +0530

    push new file

---
+++ C++/basics/patterns.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // pattern 1
+
+    cout << "Enter the value of n: " << endl;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    // pattern2
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    // pattern3
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    // pattern4
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+
+    // pattern5
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // pattern6
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j <= n - i + 1; j++) // here +1 is added due to int starting from 1, hence to cancel it out
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    // pattern7
+}
